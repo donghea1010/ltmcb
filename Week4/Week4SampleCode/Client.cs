9e34ddc8883344b35f8a614c182fcf3879dcc3f5
donghea1010 <covanh0511@gmail.com>
2023-06-19 19:37:10 +0700

    no message

---
+++ Week4/Week4SampleCode/Client.cs
@@ -0,0 +1,127 @@
+﻿
+using System.Net.Sockets;
+using System.Net;
+
+namespace Week4SampleCode
+{
+    public partial class Client : Form
+    {
+        public Client()
+        {
+            InitializeComponent();
+        }
+
+        private TcpClient tcpClient;
+        private string data;
+        //private NetworkStream ns;
+        private const int serverPort = 8080;
+        private Thread clientThread;
+        private StreamReader sReader;
+        private StreamWriter sWriter;
+        private bool stoptcpClient = true;
+
+        private delegate void SafeCallDelegate(string text);
+        private void UpdateChatHistorySafeCall(string text)
+        {
+            if (msgBox.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(UpdateChatHistorySafeCall);
+                msgBox.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                msgBox.Text += text + "\n";
+            }
+        }
+
+        private void ClientReceive()
+        {
+            sReader = new StreamReader(tcpClient.GetStream());
+
+            while (!stoptcpClient && tcpClient.Connected)
+            {
+                // Application.DoEvents();
+                string rcvdata = sReader.ReadLine();
+                UpdateChatHistorySafeCall(rcvdata);
+            }
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                stoptcpClient = false;
+
+                tcpClient = new TcpClient();
+
+
+                // 1. Create IP endpoint
+                string serverIP = "127.0.0.1"; // Replace with your server IP address
+                IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse(serverIP), serverPort);
+
+                // 2. Call connect function of TCPClient class
+                tcpClient.Connect(serverEndPoint);
+
+                // 3. using streamwrite and tcpClient.GetStream()
+                NetworkStream clientStream = tcpClient.GetStream();
+                sWriter = new StreamWriter(clientStream);
+                sWriter.AutoFlush = true;
+
+                // 4. using variable sWriter to write username into stream
+                string username = "YourUsername"; // Replace with the desired username
+                sWriter.WriteLine(username);
+
+                // 5. Create new Thread(ClientReceive)
+                clientThread = new Thread(ClientReceive);
+
+                // 6. Start Thread
+                clientThread.Start();
+
+                MessageBox.Show("Connected");
+                connectButton.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                data = sendMsgBox.Text;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                data = sendMsgBox.Text;
+                //7. using streamwriter to send data (readline function)
+                
+
+                sendMsgBox.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void disconnectButton_Click(object sender, EventArgs e)
+        {
+            stoptcpClient = true;
+            clientThread = null;
+            tcpClient.Close();
+
+            MessageBox.Show("Disconnected.");
+        }
+    }
+}
---
+++ Week4/Week4SampleCode/Server.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week4SampleCode
+{
+    public partial class Server : Form
+    {
+        public Server()
+        {
+            InitializeComponent();
+        }
+        private Thread listenThread;
+        private TcpListener tcpListener;
+        private bool stopChatServer = true;
+        private readonly int _serverPort = 8080;
+        private Dictionary<string, TcpClient> dict = new Dictionary<string, TcpClient>();
+
+        public void Listen()
+        {
+            try
+            {
+                // 1. new and start a tcpListener
+                IPAddress ipAddress = IPAddress.Parse("127.0.0.1"); // Replace with your desired IP address
+                tcpListener = new TcpListener(ipAddress, _serverPort);
+                tcpListener.Start();
+
+                while (!stopChatServer)
+                {
+                    // 1.1. create a TcpClient = tcpListener.AcceptTcpClient()
+                    TcpClient tcpClient = tcpListener.AcceptTcpClient();
+
+                    // 2. create a streamReader and StreamWriter; getStream from tcpClient in step 1
+                    NetworkStream clientStream = tcpClient.GetStream();
+                    StreamReader sReader = new StreamReader(clientStream);
+                    StreamWriter sWriter = new StreamWriter(clientStream);
+                    sWriter.AutoFlush = true;
+
+                    // 3. Using streamreader read stream to get username which is sent by client.
+                    string username = sReader.ReadLine();
+
+                    // 4. check if username exists by dictionary.containskey(username)
+                    if (dict.ContainsKey(username))
+                    {
+                        // 4.1 if username exists using streamwriter to write message username already exists to client.
+                        sWriter.WriteLine("Username already exists.");
+                    }
+                    else
+                    {
+                        // 4.2 if username not exists: add username into dictionary (dict variable)
+                        dict.Add(username, tcpClient);
+
+                        // then create and start a thread to handle client receive data
+                        Thread clientThread = new Thread(() => ClientRecv(username, tcpClient));
+                        clientThread.Start();
+                    }
+                }
+            }
+            catch (SocketException sockEx)
+            {
+                MessageBox.Show(sockEx.Message);
+            }
+        }
+
+        public void ClientRecv(string username, TcpClient tcpClient)
+        {
+            // 5. create a streamReader and getstream from tcpClient
+            NetworkStream clientStream = tcpClient.GetStream();
+            StreamReader sReader = new StreamReader(clientStream);
+
+            while (!stopChatServer)
+            {
+                // 6. using ReadLine() from streamReader to read content of stream
+                string receivedData = sReader.ReadLine();
+
+                foreach (TcpClient otherClient in dict.Values)
+                {
+                    // 7. using streamwriter to send data to other clients
+                    NetworkStream otherClientStream = otherClient.GetStream();
+                    StreamWriter otherClientWriter = new StreamWriter(otherClientStream);
+                    otherClientWriter.AutoFlush = true;
+                    otherClientWriter.WriteLine(username + ": " + receivedData);
+                }
+
+                // 8. call UpdateChatHistorySafeCall function to update UI with the msg
+                UpdateChatHistorySafeCall(username + ": " + receivedData);
+            }
+        }
+
+        private void listenButton_Click(object sender, EventArgs e)
+        {
+            if (stopChatServer)
+            {
+                stopChatServer = false;
+                listenThread = new Thread(new ThreadStart(Listen));
+                listenThread.Start();
+                MessageBox.Show("Listening...");
+
+                listenButton.Text = "Stop";
+            }
+            else
+            {
+                stopChatServer = true;
+
+                tcpListener.Stop();
+                listenThread = null;
+                listenButton.Text = "Listen";
+            }
+        }
+
+
+    }
+}
+
+        private void listenButton_Click(object sender, EventArgs e)
+        {
+            if (stopChatServer)
+            {
+                stopChatServer = false;
+                //0. create a listerThread
+                //listenThread = new Thread(new ThreadStart(Listen));
+                //listenThread.Start();
+                //MessageBox.Show("Listening...");
+
+                listenButton.Text = @"Stop";
+            }
+            else
+            {
+                stopChatServer = true;
+
+                tcpListener.Stop();
+                listenThread = null;
+                listenButton.Text = @"Listen";
+            }
+        }
+    }
+}
+
